c6240556086e1c3ebc5335545e3cd14b75964794
OlavOlsm <OlavOlsm@gmail.com>
2015-10-17 18:01:22 +0200

    create properties and constructor

--- Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/Board.cs
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/Board.cs
@@ -7,5 +7,17 @@ namespace SnakeMess
 {
     class Board
     {
+        // Create properties for console height and width
+        public int boardHeight { get; }
+        public int boardWidth { get; }
+
+        // Construct board options
+        public Board()
+        {
+            boardWidth = Console.WindowWidth;
+            boardHeight = Console.WindowHeight;
+            Console.CursorVisible = false;
+            Console.Title = "Westerdals Oslo ACT - SNAKE";
+        }
     }
 }
