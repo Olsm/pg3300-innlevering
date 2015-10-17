4cf7a139e5bb193a10eb8664f5c93fac6afda1e6
OlavOlsm <OlavOlsm@gmail.com>
2015-10-17 17:08:40 +0200

    Create classes

---
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/Coord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess
+{
+    class Coord
+    {
+        public int X, Y;
+        public Coord(int x = 0, int y = 0) { X = x; Y = y; }
+        public Coord(Coord input) { X = input.X; Y = input.Y; }
+    }
+}
