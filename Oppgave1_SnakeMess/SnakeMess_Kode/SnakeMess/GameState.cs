fd1b71d263b408d060917f535936e97925b612b6
OlavOlsm <OlavOlsm@gmail.com>
2015-10-17 17:25:03 +0200

    Create gamestates

--- Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/GameState.cs
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/GameState.cs
@@ -7,5 +7,17 @@ namespace SnakeMess
 {
     class GameState
     {
+        // Create variables for states
+        public bool pause { get; set; }
+        public bool dollarFound { get; set; }
+        public bool spawnDollar { get; set; }
+
+        // Construct GameState and instantiate initial states
+        public GameState ()
+        {
+            pause = false;
+            dollarFound = false;
+            spawnDollar = true;
+        }
     }
 }
