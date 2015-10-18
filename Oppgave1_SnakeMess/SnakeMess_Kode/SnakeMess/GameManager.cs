77612fdfe66117c320934c9c33fd6c91f52635cd
OlavOlsm <OlavOlsm@gmail.com>
2015-10-18 18:42:52 +0200

    fix movement, add dollar hit

--- Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/GameManager.cs
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/GameManager.cs
@@ -12,11 +12,10 @@ namespace SnakeMess
     {
         public GameState state;
         public Direction snakeDirection { get; set; }
-        private Board board;
+        public Board board;
         public List<Coord> snakePosition;
-        private Coord dollarPosition;
+        public Coord dollarPosition;
         private Random randomGenerator;
-        private Stopwatch timer;
 
         internal enum Direction
         {
@@ -30,12 +29,6 @@ namespace SnakeMess
             snakeDirection = new Direction();
             dollarPosition = new Coord();
             randomGenerator = new Random();
-            timer = new Stopwatch();
-        }
-
-        public void startTimer()
-        {
-            timer.Start();
         }
 
         // Add the game elements to console
@@ -46,18 +39,23 @@ namespace SnakeMess
 
             // Create snake element positions
             for (int i = 0; i < 5; i++)
-                snakePosition.Add(new Coord(10, 10));
+                snakePosition.Add(new Coord(10, 10 + i));
 
             // Add yellow snake elements
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.SetCursorPosition(10, 10);
-            Console.Write("@");
-           
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(10, 10 + i);
+                if (i > 0) Console.Write("0");
+                else Console.Write("X");
+            }
+
             // Spawn a dollar at random position on screen
             spawnDollar();
 
         }
 
+        // End gaming using Environment.exit for closing console app
         public void endGame()
         {
             Environment.Exit(0);
@@ -66,10 +64,10 @@ namespace SnakeMess
         public void moveSnake(Direction direction)
         {
             snakeDirection = direction;
-
             int addX = 0;
             int addY = 0;
 
+            // Move position 1 pixel to direction
             if (direction == Direction.Down)
                 addY = 1;
             else if (direction == Direction.Up)
@@ -78,20 +76,31 @@ namespace SnakeMess
                 addX = -1;
             else if (direction == Direction.Right)
                 addX = 1;
+            
+            // Get position of head and add x or y pixel
+            int newX = snakePosition.ElementAt(0).X + addX;
+            int newY = snakePosition.ElementAt(0).Y + addY;
 
+            // Get position of head to set first body element
+            int bodyX = snakePosition.ElementAt(0).X;
+            int bodyY = snakePosition.ElementAt(0).Y;
+
+            // Get position of last element (tail) and remove it
+            int tailX = snakePosition.ElementAt(snakePosition.Count - 1).X;
+            int tailY = snakePosition.ElementAt(snakePosition.Count - 1).Y;
+            Console.SetCursorPosition(tailX, tailY);
+            Console.Write(" ");
+            snakePosition.RemoveAt(snakePosition.Count - 1);
+
+            // Add the new head to first element
+            snakePosition.Insert(0, new Coord(newX, newY));
+            Console.SetCursorPosition(newX, newY);
             Console.ForegroundColor = ConsoleColor.Yellow;
-
-            for (int i = 0; i < snakePosition.Count; i++)
-            {
-                snakePosition.Insert(i, new Coord(snakePosition.ElementAt(i).X + addX, snakePosition.ElementAt(i).Y + addY));
-                snakePosition.RemoveAt(i + 1);
-                Console.SetCursorPosition(snakePosition.ElementAt(i).X, snakePosition.ElementAt(i).Y);
-                Console.Write("@");
-
-                Console.SetCursorPosition(snakePosition.ElementAt(snakePosition.Count - 1).X, snakePosition.ElementAt(snakePosition.Count - 1).X);
-                Console.Write(" ");
-            }
-
+            Console.Write("X");
+            
+            // Overwrite character of second element to body character
+            Console.SetCursorPosition(bodyX, bodyY);
+            Console.Write("0");
         }
 
         public void spawnDollar()
@@ -104,6 +113,32 @@ namespace SnakeMess
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(dollarPosition.X, dollarPosition.Y);
             Console.Write("$");
+        }
+
+        public void dollarHit()
+        {
+            int addX = 0;
+            int addY = 0;
+
+            // Figure out where to add the head element according to movement
+            if (snakeDirection == Direction.Up)
+                addY = 1;
+            else if (snakeDirection == Direction.Down)
+                addY = -1;
+            else if (snakeDirection == Direction.Left)
+                addX = 1;
+            else if (snakeDirection == Direction.Right)
+                addX = -1;
+
+            int newX = snakePosition.ElementAt(snakePosition.Count - 1).X + addX;
+            int newY = snakePosition.ElementAt(snakePosition.Count - 1).Y + addY;
+
+            // Add the new head element
+            snakePosition.Add(new Coord(newX, newY));
+            Console.SetCursorPosition(newX, newY);
+            Console.Write("@");
+
+            spawnDollar();
         }
     }
 }
