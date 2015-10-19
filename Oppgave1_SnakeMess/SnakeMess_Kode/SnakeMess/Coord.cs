34b849189fc966983048cdcda00a955528873843
OlavOlsm <OlavOlsm@gmail.com>
2015-10-19 14:45:40 +0200

    prettify code and remove code bloat

--- Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/Coord.cs
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/Coord.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SnakeMess
+﻿namespace SnakeMess
 {
     class Coord
     {
-        public int X, Y;
-        public Coord(int x = 0, int y = 0) { X = x; Y = y; }
-        public Coord(Coord input) { X = input.X; Y = input.Y; }
+        public int x, y;
+        
+        public Coord (Coord input) {
+            x = input.x;
+            y = input.y;
+        }
+
+        public Coord(int x = 0, int y = 0) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
