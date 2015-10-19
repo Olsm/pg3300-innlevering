31cfe7f630c7f8e3949434233c1aebc1d1b3bcab
OlavOlsm <OlavOlsm@gmail.com>
2015-10-19 20:24:14 +0200

    Make properties PascalCase & use Thread.Sleep instead of timer

--- Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/SnakeMess.cs
+++ Oppgave1_SnakeMess/SnakeMess_Kode/SnakeMess/SnakeMess.cs
@@ -12,20 +12,14 @@ namespace SnakeMess
         // Main method for game
         public static void Main(string[] arguments)
 		{
-            g.createGame();
-            g.snakeDirection = Direction.Down;
-            g.timer.Start ();
+            g.CreateGame();
+            g.SnakeDirection = Direction.Down;
+            g.Timer.Start ();
 
             while (true)
             {
-                if (g.timer.ElapsedMilliseconds < 100)
-                    continue;
-                else
-                    g.timer.Restart ();
-
-                /* or use sleep
-                System.Threading.Thread.Sleep (100);
-                */
+                // Each round should take minimum 100 ms
+                Thread.Sleep (100);
                 
                 // Do stuff when key has been clicked
                 if (Console.KeyAvailable)
@@ -34,21 +28,21 @@ namespace SnakeMess
 
                     // End or pause game if escape / space clicked
                     if (cki.Key == ConsoleKey.Escape)
-                        g.endGame();
+                        g.EndGame();
                     else if (cki.Key == ConsoleKey.Spacebar)
-                        g.state.pause = !g.state.pause;
+                        g.State.pause = !g.State.pause;
 
                     // Only change direction if game is not paused
-                    if (!g.state.pause) {
+                    if (!g.State.pause) {
                         // Only allow going up if not going down, and left if not going right etc...
-                        if (cki.Key == ConsoleKey.UpArrow && g.snakeDirection != Direction.Down)
-                            g.moveSnake(Direction.Up);
-                        else if (cki.Key == ConsoleKey.DownArrow && g.snakeDirection != Direction.Up)
-                            g.moveSnake(Direction.Down);
-                        else if (cki.Key == ConsoleKey.LeftArrow && g.snakeDirection != Direction.Right)
-                            g.moveSnake(Direction.Left);
-                        else if (cki.Key == ConsoleKey.RightArrow && g.snakeDirection != Direction.Left)
-                            g.moveSnake(Direction.Right);
+                        if (cki.Key == ConsoleKey.UpArrow && g.SnakeDirection != Direction.Down)
+                            g.MoveSnake(Direction.Up);
+                        else if (cki.Key == ConsoleKey.DownArrow && g.SnakeDirection != Direction.Up)
+                            g.MoveSnake(Direction.Down);
+                        else if (cki.Key == ConsoleKey.LeftArrow && g.SnakeDirection != Direction.Right)
+                            g.MoveSnake(Direction.Left);
+                        else if (cki.Key == ConsoleKey.RightArrow && g.SnakeDirection != Direction.Left)
+                            g.MoveSnake(Direction.Right);
                     }
                 }
 
@@ -56,34 +50,34 @@ namespace SnakeMess
                 else {
 
                     // Restart loop if pause game is true
-                    if (g.state.pause)
+                    if (g.State.pause)
                         continue;
 
                     // Continue moving in same direction
-                    g.moveSnake (g.snakeDirection);
+                    g.MoveSnake (g.SnakeDirection);
                 }
 
-                Coord headPosition = g.snakePosition.ElementAt (0);
+                Coord headPosition = g.SnakePosition.ElementAt (0);
 
                 // Game over if head hits border
                 if (headPosition.x == -1
                     || headPosition.y == -1  
-                    || headPosition.x == g.board.boardWidth
-                    || headPosition.y == g.board.boardHeight)
-                        g.endGame();
+                    || headPosition.x == g.Board.BoardWidth
+                    || headPosition.y == g.Board.BoardHeight)
+                        g.EndGame();
 
                 // Game over if snake head hits body (cannibalism)
-                foreach (Coord snakeElement in g.snakePosition) {
+                foreach (Coord snakeElement in g.SnakePosition) {
                     if (snakeElement != headPosition
                         && snakeElement.x == headPosition.x
                         && snakeElement.y == headPosition.y)
-                            g.endGame ();
+                            g.EndGame ();
                 }
 
                 // Make snake larger if dollar hit
-                if (headPosition.x == g.dollarPosition.x &&
-                    headPosition.y == g.dollarPosition.y)
-                        g.dollarHit();
+                if (headPosition.x == g.DollarPosition.x &&
+                    headPosition.y == g.DollarPosition.y)
+                        g.DollarHit();
             }
         }
 	}
