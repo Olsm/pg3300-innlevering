5b78c5977401a527e1203111583179b81426d9f0
OlavOlsm <OlavOlsm@gmail.com>
2015-10-26 13:57:08 +0100

    create customer functions

--- Oppgave3_TheCookieBakery/TheCookieBakery_Kode/TheCookieBakery/Customer.cs
+++ Oppgave3_TheCookieBakery/TheCookieBakery_Kode/TheCookieBakery/Customer.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TheCookieBakery
 {
     class Customer
     {
+        public string name { get; set; }
+        Bakery bakery;
+
+        public Customer(String name, Bakery bakery) {
+            Thread thread = new Thread (new ThreadStart(BuyCookies));
+            this.name = name;
+            this.bakery = bakery;
+            thread.Start ();
+        }
+
+        public void BuyCookies() {
+            while (bakery.bakeryOpen) {
+            Thread.Sleep (1000);
+                if (bakery.cookies.Length > 0) {
+                    bakery.SellToCustomer (this);
+                }
+            }
+        }
     }
 }
