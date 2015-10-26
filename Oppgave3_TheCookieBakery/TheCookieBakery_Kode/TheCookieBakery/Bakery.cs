56df3cf0852de25f2781c439d258f9bef73e7f5f
OlavOlsm <OlavOlsm@gmail.com>
2015-10-26 13:56:53 +0100

    create sell to customer function

--- Oppgave3_TheCookieBakery/TheCookieBakery_Kode/TheCookieBakery/Bakery.cs
+++ Oppgave3_TheCookieBakery/TheCookieBakery_Kode/TheCookieBakery/Bakery.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TheCookieBakery
 {
     class Bakery
     {
-        public static ICookie[] cookies = new ICookie[12];
+        public ICookie[] cookies = new ICookie[12];
+        public bool bakeryOpen;
+        Object lockObject = new Object ();
 
         public Bakery()
         {
-            
+            bakeryOpen = true;
         }
 
-        public static void CreateCookie()
+        public void BakeCookies() {
+            for (int i = 0; i < 12; i++) {
+                Thread.Sleep (700);
+                cookies[i] = CreateCookie ();
+                Console.WriteLine ("Bakery made " + cookies[i].GetDescription () + " #" + i);
+            }
+            CloseBakery ();
+        }
+        
+        public void SellToCustomer(Customer customer) {
+            lock (lockObject) {
+                for (int i = 0; i < 12; i++) {
+                    if (cookies[i] != null) {
+                        Console.WriteLine ("                                 " +
+                            customer.name + " recieved " + cookies[i].GetDescription () + " " + i);
+                        cookies[i] = null;
+                    }
+                }
+            }
+        }
+
+        private ICookie CreateCookie()
         {
-            for (int i = 0; i < 4; i++)
-                cookies[i] = new BaseCookie();
-            for (int i = 4; i < 8; i++)
+            
+            //private static Random randomGenerator = new Random ();
+            //private int randomType = randomGenerator.Next (1, 4);
+            /*
+            for (int i = 0; i < 4; i++) {
+                cookies[i] = new BaseCookie ();
+            }
+            for (int i = 4; i < 8; i++) {
                 cookies[i] = new ChocolateDecorator(new BaseCookie());
-            for (int i = 9; i < 12; i++)
+            }
+                
+            for (int i = 8; i < 12; i++) {
                 cookies[i] = new RaisinDecorator(new BaseCookie());
+            }
+            */
+               
+            return new BaseCookie ();
+        }
+
+        public void CloseBakery () {
+            bakeryOpen = false;
         }
     }
 }
